58f19dab7437c52354b9c78210997f328ab31ea5
Bartuz <iamnotawowplayer@gmail.com>
2013-07-15 20:39:49 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opentable::Application.config.secret_token = '757f248311eb83efe039c9445e035bb49dbf281a4271c59d4958c469ad0d14d564a7211b744317c4045047beb20f3c35518a7a0ae222f41ce87a50bd984c904c'
